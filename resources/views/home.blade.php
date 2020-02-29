d446bec9b22a5705e0004acd524747b644dcecee
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-02-29 22:24:13 +0530

    Update Home view for Admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,59 @@
-@extends('layouts.app')
+@extends('layouts.custom-app')
+
+@section('custom-title')
+- Home @endsection
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="col-md-12">
+	@if(session('success'))
+		<div class="alert alert-success alert-dismissible fade show">
+		<button class="close" data-dismiss="alert">&times;</button>
+			{{session('success')}}
+		</div>
+	@endif
+	@if(session('error'))
+		<div class="alert alert-danger alert-dismissible fade show">
+		<button class="close" data-dismiss="alert">&times;</button>
+			{{session('error')}}
+		</div>
+	@endif
+		<div class="row">
+			<div class="col-md-6">
+				<div class="card card-body mb-2" style="box-shadow:0px 8px 8px rgba(0,0,0,0.1);">
+					<h5 class="modal-header">Student <span class="badge badge-success">{{$studentsCount}}</span></h5>
+					<div class="col-md-12 container modal-body">
+						<a href="{{action('StudentController@create')}}"><button class="btn btn-primary btn-sm mr-2">Add</button></a>
+						<button class="btn btn-primary btn-sm">View</button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="card card-body mb-2" style="box-shadow:0px 8px 8px rgba(0,0,0,0.1);">
+					<h5 class="modal-header">Staff</h5>
+					<div class="col-md-12 container modal-body">
+						<button class="btn btn-primary btn-sm mr-2">Add</button>
+						<button class="btn btn-primary btn-sm">View</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="modal-body">
+			<div class="row">
+				<div class="col-md-4 form-group">
+					<label for="AdminName" class="font-weight-bold">Name :</label>
+					<h4>{{Auth::user()->name}}</h4>
+				</div>
+				<div class="col-md-4 form-group">
+					<label for="AdminEmail" class="font-weight-bold">Email :</label>
+					<h4>{{Auth::user()->email}}</h4>
+				</div>
+				<div class="col-md-4 form-group">
+					<label for="AdminCreatedAt" class="font-weight-bold">Joined at :</label>
+					<h4>{{Auth::user()->created_at}}</h4>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
