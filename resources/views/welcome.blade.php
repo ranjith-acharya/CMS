d3c30580d21c86e07699d075e81a52379729e92e
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-02-29 13:50:28 +0530

    Authentication Scaffolding Package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap&subset=latin-ext,vietnamese" rel="stylesheet">
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Quicksand', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
