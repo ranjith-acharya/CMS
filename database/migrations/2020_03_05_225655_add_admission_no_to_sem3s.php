b50ba451afba3db2046ddc402bb657947cd3dbc5
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-03-05 23:36:13 +0530

    Add column to Database Migration for Sem3

---
+++ database/migrations/2020_03_05_225655_add_admission_no_to_sem3s.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAdmissionNoToSem3s extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sem3s', function (Blueprint $table) {
+            $table->string('admissionNo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sem3s', function (Blueprint $table) {
+            //
+        });
+    }
+}
