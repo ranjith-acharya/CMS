fc9354868dce14d8036ff9d9790c57b8b0495340
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-03-05 12:29:14 +0530

    Changes for Student & Staff Index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Student;
+use App\Staff;
 
 class HomeController extends Controller
 {
@@ -23,9 +24,19 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
+    public function index(){
 		$studentsCount = Student::count();
-        return view('home', compact('studentsCount'));
+		$staffsCount = Staff::count();
+        return view('home', compact('studentsCount', 'staffsCount'));
     }
+	public function studentIndex(){
+		$studentsCount = Student::count();
+		$students = Student::all();
+		return view('studentIndex', compact('studentsCount', 'students'));
+	}
+	public function staffIndex(){
+		$staffsCount = Staff::count();
+		$staffs = Staff::all();
+		return view('staffIndex', compact('staffsCount', 'staffs'));
+	}
 }
