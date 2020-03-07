3c8f194c758d980bf3ef70afb873909288c9004f
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-03-07 13:00:26 +0530

    Update

--- app/Student.php
+++ app/Student.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
-{
+{	
     protected $fillable = [
 		'admissionNo', 'firstName', 'fatherName', 'motherName', 'lastName', 'email', 'password', 'address', 'contact1', 'contact2', 'birth', 'age', 'gender', 'year', 'branch', 'avatar',
 	];
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,14 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/home', 'HomeController@index');
+//
 Route::get('/home/student', 'HomeController@studentIndex');
+//
 Route::get('/home/staff', 'HomeController@staffIndex');
+//
 Route::resource('/student/result', 'ResultsController');
 Route::resource('/student/result/sem3', 'Sem\Sem3Controller');
+//
 Route::resource('/student', 'StudentController');
+//
 Route::resource('/staff', 'StaffController');
