2d5c1085a7c1ae5ec04e1e456fe4611e2c2a23ac
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-03-07 14:39:15 +0530

    Update for Student model

--- app/Student.php
+++ app/Student.php
@@ -2,18 +2,44 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Student extends Model
-{	
+class Student extends Authenticatable
+{
+    use Notifiable;
+	
+	protected $guard = 'student';
+	
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
 		'admissionNo', 'firstName', 'fatherName', 'motherName', 'lastName', 'email', 'password', 'address', 'contact1', 'contact2', 'birth', 'age', 'gender', 'year', 'branch', 'avatar',
 	];
 	
-	protected $hidden = [
-        'password', 'remember_token',
-    ];
 	protected function sem3(){
 		return $this->hasOne('App\Sem3');
 	}
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+	
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
