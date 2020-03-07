a201bc9863022f574e5f2a85b4e50061c20d469b
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-03-07 14:39:02 +0530

    Update for Staff model

--- app/Staff.php
+++ app/Staff.php
@@ -2,15 +2,44 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Staff extends Model
+class Staff extends Authenticatable
 {
+    use Notifiable;
+	
+	protected $guard = 'staff';
+	
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
 		'firstName', 'fatherName', 'motherName', 'lastName', 'email', 'password', 'address', 'contact1', 'contact2', 'birth', 'age', 'gender', 'subject', 'branch', 'avatar',
 	];
 	
-	protected $hidden = [
+	protected function sem3(){
+		return $this->hasOne('App\Sem3');
+	}
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
         'password', 'remember_token',
+    ];
+	
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
