b35b3f23d10c7ea85fe41f28327532e1152a1a1c
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-03-09 16:27:22 +0530

    Update Custom Student App layout

--- resources/views/layouts/custom-StudentApp.blade.php
+++ resources/views/layouts/custom-StudentApp.blade.php
@@ -21,7 +21,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/student') }}">
+                <a class="navbar-brand" href="{{ url('/students') }}">
                     {{ config('app.name', 'CMS') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -37,7 +37,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
 						<li class="nav-item">
-							<a class="nav-link" href="{{url('/student')}}">Home</a>
+							<a class="nav-link" href="{{url('/students')}}">Home</a>
 						</li>
                         <!-- Authentication Links -->
                         @guest
