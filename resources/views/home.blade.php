525eeb4b54b4584d5921ff14a592129d20a53af3
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-05 19:36:19 +0530

    Home blade view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.custom-app')
+
+@section('options')
+<a class="dropdown-item" href="{{url('/home')}}">
+	Home
+</a>
+<a class="dropdown-item" href="{{action('HomeController@show', Auth::user()->id)}}">
+	Profile
+</a>
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in as <strong>ADMIN</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
