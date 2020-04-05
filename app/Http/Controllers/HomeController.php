f9d6977275b17204f820e3f096768e188b6fa41e
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-05 19:35:24 +0530

    Auth & Home Controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(){
+        return view('home');
+    }
+	public function show($id){
+		$user = User::find($id);
+		return view('show', compact('user', 'id'));
+	}
+}
