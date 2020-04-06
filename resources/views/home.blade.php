81b780afbbd6a5880b6d8f995bb7a1559f136efb
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-06 16:23:00 +0530

    User (ADMIN) Home blade view update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.custom-app')
 
+@section('custom-title') Home @endsection
+
 @section('options')
 <a class="dropdown-item" href="{{url('/home')}}">
 	Home
@@ -12,20 +14,37 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in as <strong>ADMIN</strong>
-                </div>
-            </div>
+        <div class="col-md-12">
+			<div class="modal-header">
+				<h5 class="mt-2">Home - (<strong>{{Auth::user()->name}}</strong>)</h5>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-5 card card-body mt-4 mr-1">
+					<div class="dropdown">
+					<strong>Students :</strong> <i class="fa fa-users"></i> {{$studentCount}}
+						<button type="button" class="btn btn-sm btn-primary dropdown-toggle float-right" data-toggle="dropdown">
+							Actions
+						</button>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="{{route('students.index')}}">All Record</a>
+							<a class="dropdown-item" href="{{route('students.create')}}">New Record</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-5 card card-body mt-4 mr-1">
+					<div class="dropdown">
+					<strong>Staffs :</strong>
+						<button type="button" class="btn btn-sm btn-primary dropdown-toggle float-right" data-toggle="dropdown">
+							Actions
+						</button>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#">Link 1</a>
+							<a class="dropdown-item" href="#">Link 2</a>
+							<a class="dropdown-item" href="#">Link 3</a>
+						</div>
+					</div>
+				</div>
+			</div>
         </div>
     </div>
 </div>
