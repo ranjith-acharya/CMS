fc459e1ae00e298bc115afdbdc6544102dc2c8cd
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-09 14:32:45 +0530

    Update App routing
    Routes include for student

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Auth::routes();
 Route::resource('/home', 'HomeController');
 Route::resource('/admin/students', 'StudentAdminController');
 Route::resource('/admin/staffs', 'StaffAdminController');
+
+Route::get('/user/logout', 'Auth\LoginController@userLogout')->name('admin.logout');
+
+
+Route::get('/student/login', 'Auth\StudentLoginController@showLoginForm')->name('student.login');
+Route::post('/student/login', 'Auth\StudentLoginController@login')->name('student.login.submit');
+Route::resource('/student', 'StudentController');
+Route::get('/student/logout', 'Auth\StudentLoginController@logout')->name('student.logout');
