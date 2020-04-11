824585a85646634dced47e4e8da0f2fbdf6e5976
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-11 15:31:05 +0530

    Update App routing

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,25 @@ Route::get('/', function () {
 
 Auth::routes();
 
+//Routing for Admin
 Route::resource('/home', 'HomeController');
 Route::resource('/admin/students', 'StudentAdminController');
+Route::get('/admin/students/results/{results}', 'StudentAdminResultController@index')->name('results.index');
+
 Route::resource('/admin/staffs', 'StaffAdminController');
 
 Route::get('/user/logout', 'Auth\LoginController@userLogout')->name('admin.logout');
 
-
+//Routing for Student
 Route::get('/student/login', 'Auth\StudentLoginController@showLoginForm')->name('student.login');
 Route::post('/student/login', 'Auth\StudentLoginController@login')->name('student.login.submit');
 Route::resource('/student', 'StudentController');
 Route::get('/student/logout', 'Auth\StudentLoginController@logout')->name('student.logout');
+
+//Routing for Staff
+Route::get('/staff/login', 'Auth\StaffLoginController@showLoginForm')->name('staff.login');
+Route::post('/staff/login', 'Auth\StaffLoginController@login')->name('staff.login.submit');
+Route::resource('/staff', 'StaffController');
+Route::get('/staff/logout', 'Auth\StaffLoginController@logout')->name('staff.logout');
+
+//
