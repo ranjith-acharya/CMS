c1e7f51ae24c9e9470240e50efb10c0e118432bb
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-13 17:24:36 +0530

    Update Student model
    Student relation with admin

--- app/Student.php
+++ app/Student.php
@@ -38,4 +38,7 @@ class Student extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+	public function user(){
+		return $this->belongsTo('App\User');
+	}
 }
