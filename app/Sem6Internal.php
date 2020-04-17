609fd30c3b9b4751fe947d029c2d4e4ff746ae27
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-17 17:11:49 +0530

    Created Sem6 Internal Model

---
+++ app/Sem6Internal.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Sem6Internal extends Authenticatable
+{
+    use Notifiable;
+
+	protected $guard = 'student';
+
+	protected $fillable = [
+		'int1', 'int1mark', 'int2', 'int2mark', 'int3', 'int3mark', 'int4', 'int4mark', 'int5', 'int5mark', 'int6', 'int6mark', 'int7', 'int7mark', 'int8', 'int8mark', 'total',  
+    ];
+	
+	public function student(){
+        return $this->belongsTo('App\Student');
+    }
+
+}
