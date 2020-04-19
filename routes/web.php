ddc3a7e058bbd38c7dd2bc5f782900892f599f30
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-19 16:03:15 +0530

    Update Application Routing

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,10 @@ Auth::routes();
 //Routing for Admin
 Route::resource('/home', 'HomeController');
 Route::resource('/admin/students', 'StudentAdminController');
-Route::get('/admin/students/results/{results}', 'StudentAdminResultController@index')->name('results.index');
-Route::get('/admin/students/results/sem6/{results}', 'Sem6\StudentAdminSem6@sem6Index');
-Route::resource('/admin/students/results/sem6/{results}/internalMarks', 'Sem6\StudentAdminSem6Int');
-Route::resource('/admin/students/results/sem6/{results}/externalMarks', 'Sem6\StudentAdminSem6Ext');
-
+Route::get('/admin/students/result/{studentID}', 'StudentAdminResultController@index')->name('results.index');
+Route::get('/admin/students/result/sem6/{studentID}', 'Sem6\StudentAdminSem6Controller@index');
+Route::resource('/admin/students/sem6/internalMarks', 'Sem6\StudentAdminSem6Int');
+Route::resource('/admin/students/sem6/externalMarks', 'Sem6\StudentAdminSem6Ext');
 Route::resource('/admin/staffs', 'StaffAdminController');
 
 Route::get('/user/logout', 'Auth\LoginController@userLogout')->name('admin.logout');
@@ -35,7 +34,7 @@ Route::get('/user/logout', 'Auth\LoginController@userLogout')->name('admin.logou
 Route::get('/student/login', 'Auth\StudentLoginController@showLoginForm')->name('student.login');
 Route::post('/student/login', 'Auth\StudentLoginController@login')->name('student.login.submit');
 Route::resource('/student', 'StudentController');
-Route::get('/student/result/{result}', 'StudentResultController@studentResult')->name('student.results.index');
+
 Route::get('/student/logout', 'Auth\StudentLoginController@logout')->name('student.logout');
 
 //Routing for Staff
