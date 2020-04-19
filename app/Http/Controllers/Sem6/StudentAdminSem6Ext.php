b03bfee392224faf6e0bc9e1e326c97f2fdfec43
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-19 15:54:40 +0530

    Sem6 Ext Controller
    Student Admin Sem6 External subjects -
    For Adding the results with,
    Subject Name & Subject Marks

--- app/Http/Controllers/Sem6/StudentAdminSem6Ext.php
+++ app/Http/Controllers/Sem6/StudentAdminSem6Ext.php
@@ -9,10 +9,6 @@ use App\Sem6External;
 
 class StudentAdminSem6Ext extends Controller
 {
-	public function __construct(){
-		$this->middleware('auth');
-	}
-	
     /**
      * Display a listing of the resource.
      *
@@ -39,7 +35,7 @@ class StudentAdminSem6Ext extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, $id)
+    public function store(Request $request)
     {
         $this->validate($request, [
 			'ext1' => 'required',
@@ -47,52 +43,49 @@ class StudentAdminSem6Ext extends Controller
 			'ext3' => 'required',
 			'ext4' => 'required',
 			'ext5' => 'required',
-			'ext1mark' => 'required|digits_between: 0,80',
-			'ext2mark' => 'required|digits_between: 0,80',
-			'ext3mark' => 'required|digits_between: 0,80',
-			'ext4mark' => 'required|digits_between: 0,80',
-			'ext5mark' => 'required|digits_between: 0,80',
+			'ext1mark' => 'required',
+			'ext2mark' => 'required',
+			'ext3mark' => 'required',
+			'ext4mark' => 'required',
+			'ext5mark' => 'required',
+			'outOfExt' => 'required',
 		],[
-			'ext1.required'=> 'Please Select the Subject',
-			'ext2.required'=> 'Please Select the Subject',
-			'ext3.required'=> 'Please Select the Subject',
-			'ext4.required'=> 'Please Select the Subject',
-			'ext5.required'=> 'Please Select the Subject',
-			'ext1mark.required'=> 'Please provide the Marks',
-			'ext2mark.required'=> 'Please provide the Marks',
-			'ext3mark.required'=> 'Please provide the Marks',
-			'ext4mark.required'=> 'Please provide the Marks',
-			'ext5mark.required'=> 'Please provide the Marks',
-			'ext1mark.digits_between'=> 'Please provide the Marks within 0 to 80',
-			'ext2mark.digits_between'=> 'Please provide the Marks within 0 to 80',
-			'ext3mark.digits_between'=> 'Please provide the Marks within 0 to 80',
-			'ext4mark.digits_between'=> 'Please provide the Marks within 0 to 80',
-			'ext5mark.digits_between'=> 'Please provide the Marks within 0 to 80',
+			'ext1.required' => 'Please select Subject',
+			'ext2.required' => 'Please select Subject',
+			'ext3.required' => 'Please select Subject',
+			'ext4.required' => 'Please select Subject',
+			'ext5.required' => 'Please select Subject',
+			'ext1mark.required' => 'Please provide Marks',
+			'ext2mark.required' => 'Please provide Marks',
+			'ext3mark.required' => 'Please provide Marks',
+			'ext4mark.required' => 'Please provide Marks',
+			'ext5mark.required' => 'Please provide Marks',
+			'outOfExt.required' => 'Please select No. of Subjects',
 		]);
-		
-		$sem6External = new Sem6External;
-		$student = Student::find($id);
-		$sem6External -> ext1 = $request->get('ext1');
-		$sem6External -> ext1mark = $request->get('ext1mark');
-		$sem6External -> ext2 = $request->get('ext2');
-		$sem6External -> ext2mark = $request->get('ext2mark');
-		$sem6External -> ext3 = $request->get('ext3');
-		$sem6External -> ext3mark = $request->get('ext3mark');
-		$sem6External -> ext4 = $request->get('ext4');
-		$sem6External -> ext4mark = $request->get('ext4mark');
-		$sem6External -> ext5 = $request->get('ext5');
-		$sem6External -> ext5mark = $request->get('ext5mark');
-		$sem6External -> ext6 = $request->get('ext6');
-		$sem6External -> ext6mark = $request->get('ext6mark');
-		$sem6External -> ext7 = $request->get('ext7');
-		$sem6External -> ext7mark = $request->get('ext7mark');
-		$sem6External -> ext8 = $request->get('ext8');
-		$sem6External -> ext8mark = $request->get('ext8mark');
-		$sem6External -> total = $request->get('totalExtMark');
-		$sem6External -> admissionNo = $student->admissionNo;
-		
-		$sem6External -> save();
-		return redirect()->back()->with('success', 'External Marks for Sem6 Stored.!!');
+			$sem6External = new Sem6External;
+			$sem6External -> ext1 = $request->get('ext1');
+			$sem6External -> ext1mark = $request->get('ext1mark');
+			$sem6External -> ext2 = $request->get('ext2');
+			$sem6External -> ext2mark = $request->get('ext2mark');
+			$sem6External -> ext3 = $request->get('ext3');
+			$sem6External -> ext3mark = $request->get('ext3mark');
+			$sem6External -> ext4 = $request->get('ext4');
+			$sem6External -> ext4mark = $request->get('ext4mark');
+			$sem6External -> ext5 = $request->get('ext5');
+			$sem6External -> ext5mark = $request->get('ext5mark');
+			$sem6External -> ext6 = $request->get('ext6');
+			$sem6External -> ext6mark = $request->get('ext6mark');
+			$sem6External -> ext7 = $request->get('ext7');
+			$sem6External -> ext7mark = $request->get('ext7mark');
+			$sem6External -> ext8 = $request->get('ext8');
+			$sem6External -> ext8mark = $request->get('ext8mark');
+			$sem6External -> total = $request->get('totalExtMark');
+			$sem6External -> outOf = $request->get('outOfExt');
+			$sem6External -> admissionNo = $request->get('admissionNo');
+			
+			$sem6External -> save();
+			
+			return redirect()->back()->with('success', 'Sem6 External marks Stored.');
     }
 
     /**
