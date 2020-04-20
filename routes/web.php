19501a3d050b230af7f39bbe9de9d0f6d3001829
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-20 15:59:31 +0530

    Update Application routing
    Updated student admin result for sem 1, 2, 3, 4, 5, 7, 8

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,46 @@ Auth::routes();
 Route::resource('/home', 'HomeController');
 Route::resource('/admin/students', 'StudentAdminController');
 Route::get('/admin/students/result/{studentID}', 'StudentAdminResultController@index')->name('results.index');
+//Sem1 Student Admin Controller
+Route::get('/admin/students/result/sem1/{studentID}', 'Sem1\StudentAdminSem1Controller@index');
+Route::resource('/admin/students/sem1/internalMarks', 'Sem1\StudentAdminSem1Int');
+Route::resource('/admin/students/sem1/externalMarks', 'Sem1\StudentAdminSem1Ext');
+//
+//Sem2 Student Admin Controller
+Route::get('/admin/students/result/sem2/{studentID}', 'Sem2\StudentAdminSem2Controller@index');
+Route::resource('/admin/students/sem2/internalMarks', 'Sem2\StudentAdminSem2Int');
+Route::resource('/admin/students/sem2/externalMarks', 'Sem2\StudentAdminSem2Ext');
+//
+//Sem3 Student Admin Controller
+Route::get('/admin/students/result/sem3/{studentID}', 'Sem3\StudentAdminSem3Controller@index');
+Route::resource('/admin/students/sem3/internalMarks', 'Sem3\StudentAdminSem3Int');
+Route::resource('/admin/students/sem3/externalMarks', 'Sem3\StudentAdminSem3Ext');
+//
+//Sem4 Student Admin Controller
+Route::get('/admin/students/result/sem4/{studentID}', 'Sem4\StudentAdminSem4Controller@index');
+Route::resource('/admin/students/sem4/internalMarks', 'Sem4\StudentAdminSem4Int');
+Route::resource('/admin/students/sem4/externalMarks', 'Sem4\StudentAdminSem4Ext');
+//
+//Sem5 Student Admin Controller
+Route::get('/admin/students/result/sem5/{studentID}', 'Sem5\StudentAdminSem5Controller@index');
+Route::resource('/admin/students/sem5/internalMarks', 'Sem5\StudentAdminSem5Int');
+Route::resource('/admin/students/sem5/externalMarks', 'Sem5\StudentAdminSem5Ext');
+//
+//Sem6 Student Admin Controller
 Route::get('/admin/students/result/sem6/{studentID}', 'Sem6\StudentAdminSem6Controller@index');
 Route::resource('/admin/students/sem6/internalMarks', 'Sem6\StudentAdminSem6Int');
 Route::resource('/admin/students/sem6/externalMarks', 'Sem6\StudentAdminSem6Ext');
+//
+//Sem7 Student Admin Controller
+Route::get('/admin/students/result/sem7/{studentID}', 'Sem7\StudentAdminSem7Controller@index');
+Route::resource('/admin/students/sem7/internalMarks', 'Sem7\StudentAdminSem7Int');
+Route::resource('/admin/students/sem7/externalMarks', 'Sem7\StudentAdminSem7Ext');
+//
+//Sem8 Student Admin Controller
+Route::get('/admin/students/result/sem8/{studentID}', 'Sem8\StudentAdminSem8Controller@index');
+Route::resource('/admin/students/sem8/internalMarks', 'Sem8\StudentAdminSem8Int');
+Route::resource('/admin/students/sem8/externalMarks', 'Sem8\StudentAdminSem8Ext');
+//
 Route::resource('/admin/staffs', 'StaffAdminController');
 
 Route::get('/user/logout', 'Auth\LoginController@userLogout')->name('admin.logout');
