624c573e646b73ea59ae7970b265e4286fb0a595
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-20 15:53:08 +0530

    Sem 2 External & Internal Migration

---
+++ database/migrations/2020_04_20_140420_create_sem2_internals_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSem2InternalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sem2_internals', function (Blueprint $table) {
+            $table->id();
+			$table->string('int1');
+			$table->string('int1mark');
+			$table->string('int2');
+			$table->string('int2mark');
+			$table->string('int3');
+			$table->string('int3mark');
+			$table->string('int4');
+			$table->string('int4mark');
+			$table->string('int5');
+			$table->string('int5mark');
+			$table->string('int6');
+			$table->string('int6mark');
+			$table->string('total');
+			$table->string('outOf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sem2_internals');
+    }
+}
