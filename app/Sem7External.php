bd548daadee8e7b42518e92dabb5ebea584d2cf6
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-20 15:52:23 +0530

    Sem 7 External & Internal Model

---
+++ app/Sem7External.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Sem7External extends Authenticatable
+{
+    use Notifiable;
+
+	protected $guard = 'student';
+
+	protected $fillable = [
+		'ext1', 'ext1mark', 'ext2', 'ext2mark', 'ext3', 'ext3mark', 'ext4', 'ext4mark', 'ext5', 'ext5mark', 'ext6', 'ext6mark', 'ext7', 'ext7mark', 'ext8', 'ext8mark', 'total', 'outOf', 
+    ];
+	
+	public function student(){
+        return $this->belongsTo('App\Student');
+    }
+
+}
