24f9c24afafb2b0a40fc894914d0cd260f9a1ffa
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-20 15:56:17 +0530

    Sem 4 External & Internal Migration
    Foreign Key Admission no

---
+++ database/migrations/2020_04_20_141557_add_admissionno_to_sem4_externals.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAdmissionnoToSem4Externals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sem4_externals', function (Blueprint $table) {
+            $table->string('admissionNo')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sem4_externals', function (Blueprint $table) {
+            //
+        });
+    }
+}
