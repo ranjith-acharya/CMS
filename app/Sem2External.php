7aaca9bc9255bdbe0dd1c58a8c2ff288c8c92d73
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-22 17:08:54 +0530

    Sem 2 Student Admin
    Model update for both Internal & External added column for remark

--- app/Sem2External.php
+++ app/Sem2External.php
@@ -13,7 +13,7 @@ class Sem2External extends Authenticatable
 	protected $guard = 'student';
 
 	protected $fillable = [
-		'ext1', 'ext1mark', 'ext2', 'ext2mark', 'ext3', 'ext3mark', 'ext4', 'ext4mark', 'ext5', 'ext5mark', 'ext6', 'ext6mark', 'total', 'outOf', 
+		'ext1', 'ext1mark', 'ext2', 'ext2mark', 'ext3', 'ext3mark', 'ext4', 'ext4mark', 'ext5', 'ext5mark', 'ext6', 'ext6mark', 'total', 'outOf', 'remark', 
     ];
 	
 	public function student(){
