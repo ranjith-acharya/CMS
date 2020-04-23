8123e4789ed586059f486e13bb98dacb7ea74bdb
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-23 19:09:52 +0530

    Sem 7 Student Admin Migration
    Migration added for storing marks in both Int & Ext

--- database/migrations/2020_04_20_140500_create_sem7_internals_table.php
+++ database/migrations/2020_04_20_140500_create_sem7_internals_table.php
@@ -15,6 +15,31 @@ class CreateSem7InternalsTable extends Migration
     {
         Schema::create('sem7_internals', function (Blueprint $table) {
             $table->id();
+			$table->string('int1');
+			$table->string('int1mark');
+			$table->string('int2');
+			$table->string('int2mark');
+			$table->string('int3');
+			$table->string('int3mark');
+			$table->string('int4');
+			$table->string('int4mark');
+			$table->string('int5');
+			$table->string('int5mark');
+			$table->string('int6')->nullable();
+			$table->string('int6mark')->nullable();
+			$table->string('int7')->nullable();
+			$table->string('int7mark')->nullable();
+			$table->string('int8')->nullable();
+			$table->string('int8mark')->nullable();
+			$table->string('int9')->nullable();
+			$table->string('int9mark')->nullable();
+			$table->string('int10')->nullable();
+			$table->string('int10mark')->nullable();
+			$table->string('int11')->nullable();
+			$table->string('int11mark')->nullable();
+			$table->string('total');
+			$table->string('outOf');
+			$table->string('remark');
             $table->timestamps();
         });
     }
