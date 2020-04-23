0efee2cae2d9f1e75372d74c0c40eb33ad46471e
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-23 19:09:12 +0530

    Sem 5 Student Admin Migration
    Migration added with columns for storing marks in both Int & Ext

--- database/migrations/2020_04_20_140645_create_sem5_externals_table.php
+++ database/migrations/2020_04_20_140645_create_sem5_externals_table.php
@@ -15,6 +15,27 @@ class CreateSem5ExternalsTable extends Migration
     {
         Schema::create('sem5_externals', function (Blueprint $table) {
             $table->id();
+			$table->string('ext1');
+			$table->string('ext1mark');
+			$table->string('ext2');
+			$table->string('ext2mark');
+			$table->string('ext3');
+			$table->string('ext3mark');
+			$table->string('ext4');
+			$table->string('ext4mark');
+			$table->string('ext5');
+			$table->string('ext5mark');
+			$table->string('ext6')->nullable();
+			$table->string('ext6mark')->nullable();
+			$table->string('ext7')->nullable();
+			$table->string('ext7mark')->nullable();
+			$table->string('ext8')->nullable();
+			$table->string('ext8mark')->nullable();
+			$table->string('ext9')->nullable();
+			$table->string('ext9mark')->nullable();
+			$table->string('total');
+			$table->string('outOf');
+			$table->string('remark');
             $table->timestamps();
         });
     }
