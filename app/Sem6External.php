50ffaca89d16380941eada5c95fd8ea5ad2d23a1
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-23 19:07:13 +0530

    Sem 6 Student Admin Model
    Model update, added column remark column in both

--- app/Sem6External.php
+++ app/Sem6External.php
@@ -13,7 +13,7 @@ class Sem6External extends Authenticatable
 	protected $guard = 'student';
 
 	protected $fillable = [
-		'ext1', 'ext1mark', 'ext2', 'ext2mark', 'ext3', 'ext3mark', 'ext4', 'ext4mark', 'ext5', 'ext5mark', 'ext6', 'ext6mark', 'ext7', 'ext7mark', 'ext8', 'ext8mark', 'total', 'outOf', 
+		'ext1', 'ext1mark', 'ext2', 'ext2mark', 'ext3', 'ext3mark', 'ext4', 'ext4mark', 'ext5', 'ext5mark', 'ext6', 'ext6mark', 'ext7', 'ext7mark', 'ext8', 'ext8mark', 'total', 'outOf', 'remark',
     ];
 	
 	public function student(){
