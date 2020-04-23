97b392a74c6af215bd9e8c255a72a981674981a4
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-23 19:05:41 +0530

    Sem 7 Student Admin
    Sem 7 Student Admin Controller update for both External & Internal, for storing marks

--- app/Http/Controllers/Sem7/StudentAdminSem7Ext.php
+++ app/Http/Controllers/Sem7/StudentAdminSem7Ext.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Sem7;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Student;
+use App\Sem7External;
 
 class StudentAdminSem7Ext extends Controller
 {
@@ -33,9 +35,67 @@ class StudentAdminSem7Ext extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+			'ext1' => 'required',
+			'ext2' => 'required',
+			'ext3' => 'required',
+			'ext4' => 'required',
+			'ext5' => 'required',
+			'ext1mark' => 'required',
+			'ext2mark' => 'required',
+			'ext3mark' => 'required',
+			'ext4mark' => 'required',
+			'ext5mark' => 'required',
+			'outOfExt' => 'required',
+			'remarkExt' => 'required',
+		],[
+			'ext1.required' => 'Please select Subject',
+			'ext2.required' => 'Please select Subject',
+			'ext3.required' => 'Please select Subject',
+			'ext4.required' => 'Please select Subject',
+			'ext5.required' => 'Please select Subject',
+			'ext1mark.required' => 'Please provide Marks',
+			'ext2mark.required' => 'Please provide Marks',
+			'ext3mark.required' => 'Please provide Marks',
+			'ext4mark.required' => 'Please provide Marks',
+			'ext5mark.required' => 'Please provide Marks',
+			'outOfExt.required' => 'Please select No. of Subjects',
+			'remarkExt.required' => 'Please select Remark',
+		]);
+			$sem7External = new Sem7External;
+			$students = Student::find($id);
+			$sem7External -> ext1 = $request->get('ext1');
+			$sem7External -> ext1mark = $request->get('ext1mark');
+			$sem7External -> ext2 = $request->get('ext2');
+			$sem7External -> ext2mark = $request->get('ext2mark');
+			$sem7External -> ext3 = $request->get('ext3');
+			$sem7External -> ext3mark = $request->get('ext3mark');
+			$sem7External -> ext4 = $request->get('ext4');
+			$sem7External -> ext4mark = $request->get('ext4mark');
+			$sem7External -> ext5 = $request->get('ext5');
+			$sem7External -> ext5mark = $request->get('ext5mark');
+			$sem7External -> ext6 = $request->get('ext6');
+			$sem7External -> ext6mark = $request->get('ext6mark');
+			$sem7External -> ext7 = $request->get('ext7');
+			$sem7External -> ext7mark = $request->get('ext7mark');
+			$sem7External -> ext8 = $request->get('ext8');
+			$sem7External -> ext8mark = $request->get('ext8mark');
+			$sem7External -> ext9 = $request->get('ext9');
+			$sem7External -> ext9mark = $request->get('ext9mark');
+			$sem7External -> ext10 = $request->get('ext10');
+			$sem7External -> ext10mark = $request->get('ext10mark');
+			$sem7External -> ext11 = $request->get('ext11');
+			$sem7External -> ext11mark = $request->get('ext11mark');
+			$sem7External -> total = $request->get('totalExtMark');
+			$sem7External -> outOf = $request->get('outOfExt');
+			$sem7External -> remark = $request->get('remarkExt');
+			$sem7External -> admissionNo = $students->admissionNo;
+				
+			$sem7External -> save();
+			
+			return redirect()->back()->with('success', 'Sem7 External marks Stored.');
     }
 
     /**
--- app/Http/Controllers/Sem7/StudentAdminSem7Int.php
+++ app/Http/Controllers/Sem7/StudentAdminSem7Int.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Sem7;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Student;
+use App\Sem7Internal;
 
 class StudentAdminSem7Int extends Controller
 {
@@ -33,9 +35,67 @@ class StudentAdminSem7Int extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+			'int1' => 'required',
+			'int2' => 'required',
+			'int3' => 'required',
+			'int4' => 'required',
+			'int5' => 'required',
+			'int1mark' => 'required',
+			'int2mark' => 'required',
+			'int3mark' => 'required',
+			'int4mark' => 'required',
+			'int5mark' => 'required',
+			'outOfInt' => 'required',
+			'remarkInt' => 'required',
+		],[
+			'int1.required' => 'Please select Subject',
+			'int2.required' => 'Please select Subject',
+			'int3.required' => 'Please select Subject',
+			'int4.required' => 'Please select Subject',
+			'int5.required' => 'Please select Subject',
+			'int1mark.required' => 'Please provide Marks',
+			'int2mark.required' => 'Please provide Marks',
+			'int3mark.required' => 'Please provide Marks',
+			'int4mark.required' => 'Please provide Marks',
+			'int5mark.required' => 'Please provide Marks',
+			'outOfInt.required' => 'Please select No. of Subjects',
+			'remarkInt.required' => 'Please select Remark',
+		]);
+			$sem7Internal = new Sem7Internal;
+			$students = Student::find($id);
+			$sem7Internal -> int1 = $request->get('int1');
+			$sem7Internal -> int1mark = $request->get('int1mark');
+			$sem7Internal -> int2 = $request->get('int2');
+			$sem7Internal -> int2mark = $request->get('int2mark');
+			$sem7Internal -> int3 = $request->get('int3');
+			$sem7Internal -> int3mark = $request->get('int3mark');
+			$sem7Internal -> int4 = $request->get('int4');
+			$sem7Internal -> int4mark = $request->get('int4mark');
+			$sem7Internal -> int5 = $request->get('int5');
+			$sem7Internal -> int5mark = $request->get('int5mark');
+			$sem7Internal -> int6 = $request->get('int6');
+			$sem7Internal -> int6mark = $request->get('int6mark');
+			$sem7Internal -> int7 = $request->get('int7');
+			$sem7Internal -> int7mark = $request->get('int7mark');
+			$sem7Internal -> int8 = $request->get('int8');
+			$sem7Internal -> int8mark = $request->get('int8mark');
+			$sem7Internal -> int9 = $request->get('int9');
+			$sem7Internal -> int9mark = $request->get('int9mark');
+			$sem7Internal -> int10 = $request->get('int10');
+			$sem7Internal -> int10mark = $request->get('int10mark');
+			$sem7Internal -> int11 = $request->get('int11');
+			$sem7Internal -> int11mark = $request->get('int11mark');
+			$sem7Internal -> total = $request->get('totalIntMark');
+			$sem7Internal -> outOf = $request->get('outOfInt');
+			$sem7Internal -> remark = $request->get('remarkInt');
+			$sem7Internal -> admissionNo = $students->admissionNo;
+				 
+			$sem7Internal -> save();
+			
+			return redirect()->back()->with('success', 'Sem7 Internal marks Stored.');
     }
 
     /**
