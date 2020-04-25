f9032c5b5ddf5dbc4b99df3f8a6b98884d151c5d
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-25 21:55:25 +0530

    Sem 5 both Ext & Int
    Controller update for Student Admin in sem 5 both Internal & External -
    - Edit & Update
    - View.

--- app/Http/Controllers/Sem5/StudentAdminSem5Ext.php
+++ app/Http/Controllers/Sem5/StudentAdminSem5Ext.php
@@ -89,7 +89,11 @@ class StudentAdminSem5Ext extends Controller
 			$sem5External -> total = $request->get('totalExtMark');
 			$sem5External -> outOf = $request->get('outOfExt');
 			$sem5External -> remark = $request->get('remarkExt');
+			$sem5External -> studentId = $students->id;
 			$sem5External -> admissionNo = $students->admissionNo;
+			$sem5External -> firstName = $students->firstName;
+			$sem5External -> lastName = $students->lastName;
+			$sem5External -> branch = $students->branch;
 				
 			$sem5External -> save();
 			
@@ -104,7 +108,8 @@ class StudentAdminSem5Ext extends Controller
      */
     public function show($id)
     {
-        //
+        $sem5External = Sem5External::find($id);
+		return view('result.sem5.studentAdminSem5ExtShow', compact('sem5External', 'id'));
     }
 
     /**
@@ -115,7 +120,8 @@ class StudentAdminSem5Ext extends Controller
      */
     public function edit($id)
     {
-        //
+        $sem5External = Sem5External::find($id);
+		return view('result.sem5.studentAdminSem5ExtEdit', compact('sem5External', 'id'));
     }
 
     /**
@@ -127,7 +133,60 @@ class StudentAdminSem5Ext extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+			'ext1' => 'required',
+			'ext2' => 'required',
+			'ext3' => 'required',
+			'ext4' => 'required',
+			'ext5' => 'required',
+			'ext1mark' => 'required',
+			'ext2mark' => 'required',
+			'ext3mark' => 'required',
+			'ext4mark' => 'required',
+			'ext5mark' => 'required',
+			'outOfExt' => 'required',
+			'remarkExt' => 'required',
+		],[
+			'ext1.required' => 'Please select Subject',
+			'ext2.required' => 'Please select Subject',
+			'ext3.required' => 'Please select Subject',
+			'ext4.required' => 'Please select Subject',
+			'ext5.required' => 'Please select Subject',
+			'ext1mark.required' => 'Please provide Marks',
+			'ext2mark.required' => 'Please provide Marks',
+			'ext3mark.required' => 'Please provide Marks',
+			'ext4mark.required' => 'Please provide Marks',
+			'ext5mark.required' => 'Please provide Marks',
+			'outOfExt.required' => 'Please select No. of Subjects',
+			'remarkExt.required' => 'Please select Remark',
+		]);
+			$sem5External = Sem5External::find($id);
+			
+			$sem5External -> ext1 = $request->get('ext1');
+			$sem5External -> ext1mark = $request->get('ext1mark');
+			$sem5External -> ext2 = $request->get('ext2');
+			$sem5External -> ext2mark = $request->get('ext2mark');
+			$sem5External -> ext3 = $request->get('ext3');
+			$sem5External -> ext3mark = $request->get('ext3mark');
+			$sem5External -> ext4 = $request->get('ext4');
+			$sem5External -> ext4mark = $request->get('ext4mark');
+			$sem5External -> ext5 = $request->get('ext5');
+			$sem5External -> ext5mark = $request->get('ext5mark');
+			$sem5External -> ext6 = $request->get('ext6');
+			$sem5External -> ext6mark = $request->get('ext6mark');
+			$sem5External -> ext7 = $request->get('ext7');
+			$sem5External -> ext7mark = $request->get('ext7mark');
+			$sem5External -> ext8 = $request->get('ext8');
+			$sem5External -> ext8mark = $request->get('ext8mark');
+			$sem5External -> ext9 = $request->get('ext9');
+			$sem5External -> ext9mark = $request->get('ext9mark');
+			$sem5External -> total = $request->get('totalExtMark');
+			$sem5External -> outOf = $request->get('outOfExt');
+			$sem5External -> remark = $request->get('remarkExt');
+			
+			$sem5External -> save();
+			
+			return redirect()->back()->with('success', 'Sem5 External marks Updated.!!');
     }
 
     /**
--- app/Http/Controllers/Sem5/StudentAdminSem5Int.php
+++ app/Http/Controllers/Sem5/StudentAdminSem5Int.php
@@ -89,7 +89,11 @@ class StudentAdminSem5Int extends Controller
 			$sem5Internal -> total = $request->get('totalIntMark');
 			$sem5Internal -> outOf = $request->get('outOfInt');
 			$sem5Internal -> remark = $request->get('remarkInt');
+			$sem5Internal -> studentId = $students->id;
 			$sem5Internal -> admissionNo = $students->admissionNo;
+			$sem5Internal -> firstName = $students->firstName;
+			$sem5Internal -> lastName = $students->lastName;
+			$sem5Internal -> branch = $students->branch;
 				
 			$sem5Internal -> save();
 			
@@ -104,7 +108,8 @@ class StudentAdminSem5Int extends Controller
      */
     public function show($id)
     {
-        //
+        $sem5Internal = Sem5Internal::find($id);
+		return view('result.sem5.studentAdminSem5IntShow', compact('sem5Internal', 'id'));
     }
 
     /**
@@ -115,7 +120,8 @@ class StudentAdminSem5Int extends Controller
      */
     public function edit($id)
     {
-        //
+        $sem5Internal = Sem5Internal::find($id);
+		return view('result.sem5.studentAdminSem5IntEdit', compact('sem5Internal', 'id'));
     }
 
     /**
@@ -127,7 +133,60 @@ class StudentAdminSem5Int extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+			'int1' => 'required',
+			'int2' => 'required',
+			'int3' => 'required',
+			'int4' => 'required',
+			'int5' => 'required',
+			'int1mark' => 'required',
+			'int2mark' => 'required',
+			'int3mark' => 'required',
+			'int4mark' => 'required',
+			'int5mark' => 'required',
+			'outOfInt' => 'required',
+			'remarkInt' => 'required',
+		],[
+			'int1.required' => 'Please select Subject',
+			'int2.required' => 'Please select Subject',
+			'int3.required' => 'Please select Subject',
+			'int4.required' => 'Please select Subject',
+			'int5.required' => 'Please select Subject',
+			'int1mark.required' => 'Please provide Marks',
+			'int2mark.required' => 'Please provide Marks',
+			'int3mark.required' => 'Please provide Marks',
+			'int4mark.required' => 'Please provide Marks',
+			'int5mark.required' => 'Please provide Marks',
+			'outOfInt.required' => 'Please select No. of Subjects',
+			'remarkInt.required' => 'Please select Remark',
+		]);
+			$sem5Internal = Sem5Internal::find($id);
+			
+			$sem5Internal -> int1 = $request->get('int1');
+			$sem5Internal -> int1mark = $request->get('int1mark');
+			$sem5Internal -> int2 = $request->get('int2');
+			$sem5Internal -> int2mark = $request->get('int2mark');
+			$sem5Internal -> int3 = $request->get('int3');
+			$sem5Internal -> int3mark = $request->get('int3mark');
+			$sem5Internal -> int4 = $request->get('int4');
+			$sem5Internal -> int4mark = $request->get('int4mark');
+			$sem5Internal -> int5 = $request->get('int5');
+			$sem5Internal -> int5mark = $request->get('int5mark');
+			$sem5Internal -> int6 = $request->get('int6');
+			$sem5Internal -> int6mark = $request->get('int6mark');
+			$sem5Internal -> int7 = $request->get('int7');
+			$sem5Internal -> int7mark = $request->get('int7mark');
+			$sem5Internal -> int8 = $request->get('int8');
+			$sem5Internal -> int8mark = $request->get('int8mark');
+			$sem5Internal -> int9 = $request->get('int9');
+			$sem5Internal -> int9mark = $request->get('int9mark');
+			$sem5Internal -> total = $request->get('totalIntMark');
+			$sem5Internal -> outOf = $request->get('outOfInt');
+			$sem5Internal -> remark = $request->get('remarkInt');
+				
+			$sem5Internal -> save();
+			
+			return redirect()->back()->with('success', 'Sem5 Internal marks Updated.!!');
     }
 
     /**
