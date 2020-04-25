86a3ee3ba761e2b56da0f0d8f37820cd753ad4f6
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-25 22:00:37 +0530

    Sem 7 Int & Ext Migration
    Migration update for added foregin key

--- database/migrations/2020_04_20_141624_add_admissionno_to_sem7_internals.php
+++ database/migrations/2020_04_20_141624_add_admissionno_to_sem7_internals.php
@@ -15,6 +15,10 @@ class AddAdmissionnoToSem7Internals extends Migration
     {
         Schema::table('sem7_internals', function (Blueprint $table) {
             $table->string('admissionNo')->unique();
+			$table->string('studentId');
+			$table->string('firstName');
+			$table->string('lastName');
+			$table->string('branch');
         });
     }
 
--- database/migrations/2020_04_20_141631_add_admissionno_to_sem7_externals.php
+++ database/migrations/2020_04_20_141631_add_admissionno_to_sem7_externals.php
@@ -15,6 +15,10 @@ class AddAdmissionnoToSem7Externals extends Migration
     {
         Schema::table('sem7_externals', function (Blueprint $table) {
             $table->string('admissionNo')->unique();
+			$table->string('studentId');
+			$table->string('firstName');
+			$table->string('lastName');
+			$table->string('branch');
         });
     }
 
