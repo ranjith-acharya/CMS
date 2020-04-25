4c89a92234c89e2753e80e5a35b9fbc254776168
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-25 22:12:07 +0530

    Sem 1 Ext & Int show blade

--- resources/views/Result/Sem1/studentAdminSem1ExtShow.blade.php
+++ resources/views/Result/Sem1/studentAdminSem1ExtShow.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.custom-app')
 
-@section('custom-title') Sem 1 Internal Marks @endsection
+@section('custom-title') Sem 1 External Marks @endsection
 
 @section('options')
 <a class="dropdown-item" href="{{url('/home')}}">
@@ -16,11 +16,11 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
 			<div class="modal-header">
-				<h5 class="mt-2">Sem 1 Internal Marks</h5>
+				<h5 class="mt-2">Sem 1 External Marks</h5>
 				<div class="dropdown mt-1">
 					<button type="button" class="btn btn-sm btn-primary dropdown-toggle float-right" data-toggle="dropdown">Actions</button>
 					<div class="dropdown-menu">
-						<a class="dropdown-item" href="{{action('StudentAdminResultController@sem1Int')}}">Home</a>
+						<a class="dropdown-item" href="{{action('StudentAdminResultController@sem1Ext')}}">Home</a>
 						<a class="dropdown-item" href="{{route('staffs.index')}}">Staff Index</a>
 						<a class="dropdown-item" href="{{route('students.index')}}">Student Index</a>
 					</div>
@@ -28,8 +28,8 @@
 			</div>
 			<div class="modal-body">
 				<div class="col-md-6">
-					{{$sem1Internal->firstName}}&nbsp;{{$sem1Internal->lastName}},&nbsp;<span class="text-primary">{{$sem1Internal->admissionNo}}</span><br>
-					<span class="text-capitalize">{{$sem1Internal->branch}}&nbsp;Engg.</span><br>
+					{{$sem1External->firstName}}&nbsp;{{$sem1External->lastName}},&nbsp;<span class="text-primary">{{$sem1External->admissionNo}}</span><br>
+					<span class="text-capitalize">{{$sem1External->branch}}&nbsp;Engg.</span><br>
 				</div>
 				<div class="table-responsive mt-4">
 					<table class="table table-striped table-bordered">
@@ -41,41 +41,41 @@
 						</thead>
 						<tbody>
 							<tr>
-								<td>{{$sem1Internal->int1}}</td>
-								<td>{{$sem1Internal->int1mark}}</td>
+								<td>{{$sem1External->ext1}}</td>
+								<td>{{$sem1External->ext1mark}}</td>
 							</tr>
 							<tr>
-								<td>{{$sem1Internal->int2}}</td>
-								<td>{{$sem1Internal->int2mark}}</td>
+								<td>{{$sem1External->ext2}}</td>
+								<td>{{$sem1External->ext2mark}}</td>
 							</tr>
 							<tr>
-								<td>{{$sem1Internal->int3}}</td>
-								<td>{{$sem1Internal->int3mark}}</td>
+								<td>{{$sem1External->ext3}}</td>
+								<td>{{$sem1External->ext3mark}}</td>
 							</tr>
 							<tr>
-								<td>{{$sem1Internal->int4}}</td>
-								<td>{{$sem1Internal->int4mark}}</td>
+								<td>{{$sem1External->ext4}}</td>
+								<td>{{$sem1External->ext4mark}}</td>
 							</tr>
 							<tr>
-								<td>{{$sem1Internal->int5}}</td>
-								<td>{{$sem1Internal->int5mark}}</td>
+								<td>{{$sem1External->ext5}}</td>
+								<td>{{$sem1External->ext5mark}}</td>
 							</tr>
 							<tr>
-								<td>{{$sem1Internal->int6}}</td>
-								<td>{{$sem1Internal->int6mark}}</td>
+								<td>{{$sem1External->ext6}}</td>
+								<td>{{$sem1External->ext6mark}}</td>
 							</tr>
 						</tbody>
 						<tfoot>
 							<tr>
 								<th>Total</th>
-								<th>{{$sem1Internal->total}}</th>
+								<th>{{$sem1External->total}}</th>
 							</tr>
 							<tr>
-								@if($sem1Internal->remark == 'Pass')
-									<th colspan="2" class="text-center text-success">{{$sem1Internal->remark}} <span id="cgpaInt"></span></th>
+								@if($sem1External->remark == 'Pass')
+									<th colspan="2" class="text-center text-success">{{$sem1External->remark}} <span id="cgpaExt"></span></th>
 								@endif
-								@if($sem1Internal->remark == 'Fail')
-									<th colspan="2" class="text-center text-danger">{{$sem1Internal->remark}} <span>0.0</span></th>
+								@if($sem1External->remark == 'Fail')
+									<th colspan="2" class="text-center text-danger">{{$sem1External->remark}} <span>0.0</span></th>
 								@endif
 							</tr>
 						</tfoot>
@@ -90,17 +90,17 @@
 @section('custom-js')
 <!--<script>
 $(document).ready(function(){
-	//alert('{{$sem1Internal->admissionNo}}');
-	var total = {{$sem1Internal->total}};
+	//alert('{{$sem1External->admissionNo}}');
+	var total = {{$sem1External->total}};
 	//alert(total);
-	var outOfInt = {{$sem1Internal->outOf}};
+	var outOfInt = {{$sem1External->outOf}};
 	//alert(outOfInt);
 	var percentage = total*100/outOfInt;
 	//alert(percentage);
 	var cgpa = percentage/9.5;
 	var finalCgpa = cgpa.toFixed(2);
 	//alert(finalCgpa);
-	document.getElementById('cgpaInt').innerHTML = finalCgpa;
+	document.getElementById('cgpaExt').innerHTML = finalCgpa;
 });
 </script>-->
 @endsection
