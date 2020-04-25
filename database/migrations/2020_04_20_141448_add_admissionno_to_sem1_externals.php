c9f27dbdd209e86bd1573f103487b2d40283a7be
Ranjith Krishna Acharya <ranjithacharya997@gmail.com>
2020-04-25 22:02:07 +0530

    Sem 1 Ext Migration
    Migration update for added foregin key

--- database/migrations/2020_04_20_141448_add_admissionno_to_sem1_externals.php
+++ database/migrations/2020_04_20_141448_add_admissionno_to_sem1_externals.php
@@ -15,6 +15,10 @@ class AddAdmissionnoToSem1Externals extends Migration
     {
         Schema::table('sem1_externals', function (Blueprint $table) {
             $table->string('admissionNo')->unique();
+			$table->string('studentId');
+			$table->string('firstName');
+			$table->string('lastName');
+			$table->string('branch');
         });
     }
 
