5f514c77c2e8312965754396a384cecf5396c671
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2020-04-30 21:30:13 +0530

    Update Studet Sem 3 Result blade view
    Script replaced

--- resources/views/Student/studentSem3.blade.php
+++ resources/views/Student/studentSem3.blade.php
@@ -100,23 +100,6 @@
 								</tr>
 							</tfoot>
 						</table>
-							<script>
-							$(document).ready(function(){
-								var intTotal = {{$sem3Int->total}};
-								var intOutof = {{$sem3Int->outOf}};
-								var extOutof = {{$sem3Ext->outOf}};
-								//alert(intOutof);
-								var extTotal = {{$sem3Ext->total}};
-								var total = intTotal + extTotal;
-								var outOf = intOutof + extOutof;
-								//alert(total);
-								document.getElementById('totalSem3').innerHTML = total+" / "+outOf;
-								var perc = total*100/outOf;
-								var cgpa = perc/9.5;
-								document.getElementById('cgpaSem3').innerHTML = cgpa.toFixed(2);
-								console.log(cgpa);
-							});
-							</script>
 						@endforeach
 					@endforeach
 					@endif
@@ -126,4 +109,24 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('custom-js')
+<script>
+$(document).ready(function(){
+	var intTotal = {{$sem3Int->total}};
+	var intOutof = {{$sem3Int->outOf}};
+	var extOutof = {{$sem3Ext->outOf}};
+	//alert(intOutof);
+	var extTotal = {{$sem3Ext->total}};
+	var total = intTotal + extTotal;
+	var outOf = intOutof + extOutof;
+	//alert(total);
+	document.getElementById('totalSem3').innerHTML = total+" / "+outOf;
+	var perc = total*100/outOf;
+	var cgpa = perc/9.5;
+	document.getElementById('cgpaSem3').innerHTML = cgpa.toFixed(2);
+	console.log(cgpa);
+});
+</script>
 @endsection
