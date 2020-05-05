b46187119b0342af8e78d5b2f3748ce6dc2945fc
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2020-05-05 17:27:52 +0530

    Update App Layout blade
    - Script Replacement
    - Added Footer with Contributors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,17 @@
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="{{asset('css/custom.css')}}">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body class="bg-light">
 <div class="container">
 	@yield('content')
-</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</div><br><br><br><br><br>
+<footer class="mt-4 pb-4 pt-4 text-white text-center" style="background-color:#2c3e50;">
+	<div>&copy; Copyright 2020. All Rights Reserved.</div>
+	<div><u><a href="https://www.ranjith-acharya.tk" class="link text-light" target="_blank">Ranjith Acharya</a></u></div>
+</footer>
 </body>
 </html>
